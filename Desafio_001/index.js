f24164924958b33b6b78dbcd16a6eeede91b7119
omairapalacios <cpalaciosanchez@hotmail.com>
2022-02-16 00:16:21 -0500

    resolve challenge 001 - Classes

---
+++ Desafio_001/index.js
@@ -0,0 +1,43 @@
+class User {
+  constructor(name, lastname, books = [], pets = []) {
+    (this.name = name),
+      (this.lastname = lastname),
+      (this.books = books),
+      (this.pets = pets);
+  }
+
+  getFullName() {
+    return `${this.name} ${this.lastname}`;
+  }
+
+  addPet(pet) {
+    this.pets = [...this.pets, pet];
+  }
+
+  countPets() {
+    return this.pets.length;
+  }
+
+  addBook(name, autor) {
+    this.books = [...this.books, { name, autor }];
+  }
+
+  getBooksNames() {
+    return this.books.map(({ name }) => name);
+  }
+}
+
+// user instance
+const user = new User('Omaira', 'Palacios');
+
+console.log(user.getFullName()); // output: Omaira Palacios
+
+user.addPet('perro');
+user.addPet('gato');
+
+console.log(user.countPets()); // output: 2
+
+user.addBook('Los ojos de mi princesa', 'Carlos Cuauhtémoc');
+user.addBook('Juventud en éxtasis', 'Carlos Cuauhtémoc');
+
+console.log(user.getBooksNames()); // output: [ 'Los ojos de mi princesa', 'Juventud en éxtasis' ]
