9321cbbc96a37b9bcfe9b8482021dcea2f0acab3
omairapalacios <cpalaciosanchez@hotmail.com>
2022-03-18 00:54:53 -0500

    add pug views

--- Desafio_005/src/index.js
+++ Desafio_005/src/index.js
@@ -9,9 +9,19 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', handlebars.engine());
-app.set('views', __dirname+'/views');
-app.set('view engine', 'handlebars');
+//HBS
+// app.engine('handlebars', handlebars.engine());
+// app.set('views', __dirname+'/views-hbs');
+// app.set('view engine', 'handlebars');
+
+//PUG
+app.set('views', __dirname+'/views-pug');
+app.set('view engine', 'pug');
+
+//EJS
+// app.set('views', __dirname+'/views-ejs');
+// app.set('view engine', 'ejs');
+
 
 app.set('port', process.env.PORT || 8080);
 
