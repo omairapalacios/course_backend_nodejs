8453d47ff5ee3f34b62ed70e68976052fc995a69
omairapalacios <cpalaciosanchez@hotmail.com>
2022-05-24 23:32:11 -0500

    add branch to resolve challenge 13

--- Desafio_010/src/models/schemaCarts.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cartsSchema = new mongoose.Schema({
-  products: { type: Array, require: false },
-  timestamp: { type: String, require: true },
-});
-
-module.exports = mongoose.model('carts', cartsSchema);
