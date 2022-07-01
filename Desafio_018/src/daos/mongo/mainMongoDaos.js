b28104acb58dcafae02d1311d62d5038b30168f8
omairapalacios <cpalaciosanchez@hotmail.com>
2022-07-01 10:01:04 -0500

    fix heroku deploy

--- Desafio_018/src/daos/mongo/mainMongoDaos.js
+++
@@ -1,4 +0,0 @@
-const cartDaos = require('./cartMongo.daos');
-const productDaos = require('./productMongo.daos');
-
-module.exports = { cartDaos, productDaos };
---
+++ src/build/precache-manifest.bbcad1284c5c89bb960c56982ae675e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2838ae16fbcdefe9dc20585c9cedc5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bedc51607610d2c2edf",
+    "url": "/static/css/main.6c361192.chunk.css"
+  },
+  {
+    "revision": "a3648fe62067ccb3d57c",
+    "url": "/static/js/2.2957713b.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/static/js/2.2957713b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116826e31f16dae72625",
+    "url": "/static/js/3.fd569966.chunk.js"
+  },
+  {
+    "revision": "6bedc51607610d2c2edf",
+    "url": "/static/js/main.810000ba.chunk.js"
+  },
+  {
+    "revision": "4c8f55b29b44b3ec327f",
+    "url": "/static/js/runtime-main.7863124d.js"
+  }
+]);
